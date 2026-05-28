55540014512d1035bc890ea1be88b21b60238d04
Akshansh246 <akshanshsaraf40@gmail.com>
2026-05-28 14:53:17 +0530

    first commit

---
+++ sandbox/server/server.js
@@ -0,0 +1,5 @@
+import app from "./src/app.js";
+
+app.listen(3000, ()=>{
+    console.log("Sandbox API server is running on Port 3000")
+})
---
+++ sandbox/server/src/app.js
@@ -0,0 +1,18 @@
+import express, { urlencoded } from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/api/sandbox/health', (req, res) => {
+    return res.status(200).json({
+        message: "Sandbox api is healthy",
+        status: 'ok'
+    });
+})
+
+export default app
